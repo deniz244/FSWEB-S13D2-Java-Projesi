a44803ddb35304549328c5c2c5f03a86fc2235b6
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-14 16:58:59 +0300

    space

--- src/Main.java
+++ src/Main.java
@@ -42,7 +42,7 @@ public class Main {
         if(number <= 0){
             return false;
         }
-        
+
         int sum = 1;
         for(int i = 2; i < number; i++){
             if(number % i == 0){
